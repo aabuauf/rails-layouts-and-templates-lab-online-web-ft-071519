72710a40d891d5baa62270c14b26eda6dff927c5
Ahmed Abououf <aabuauf@yahoo.com>
2019-09-26 19:12:27 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'static_controller/new'
+
+  get 'static_controller/create'
+
+  get 'static_controller/edit'
+
+  get 'static_controller/update'
+
+  get 'static_controller/destroy'
+
+  get 'static_controller/index'
+
+  get 'static_controller/show'
+
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
